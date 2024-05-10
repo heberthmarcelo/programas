5c8a26dd110e0aa00afbc6825315a8bb03505ddb
Heberth Marcelo <ashcrachas@gmail.com>
2024-05-09 22:21:53 -0300

    solicita o some e o sobrenome. Em seguida, imprima o nome complento no console

---
+++ exercicio1.js
@@ -0,0 +1,5 @@
+// solicita o some e o sobrenome. Em seguida, imprima o nome complento no console
+ 
+let nome = prompt("Digite p seu nome: ");
+let sobrenome = prompt("Digite o seu sobrenome: ");
+console.log("o seu nome e :" + nome + " " + sobrenome)
